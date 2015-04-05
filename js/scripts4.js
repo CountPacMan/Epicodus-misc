ff3379f4464e98c5e17b7d60f05bfcccf6e45c9c
CountPacMan <venom20xx@yahoo.com>
2015-04-05 14:40:34 -0700

    refactor madlibs

--- js/scripts4.js
+++ js/scripts4.js
@@ -1,18 +1,11 @@
 jQuery(document).ready(function() {
   $("#blanks form").submit(function(event) {
-    var person1 = $("input#person1").val();
-    var person2 = $("input#person2").val();
-    var animal = $("input#animal").val();
-    var exclamation = $("input#exclamation").val();
-    var verb = $("input#verb").val();
-    var noun = $("input#noun").val();
+    var blanks = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
 
-    $(".person1").text(person1);
-    $(".person2").text(person2);
-    $(".animal").text(animal);
-    $(".exclamation").text(exclamation);
-    $(".verb").text(verb);
-    $(".noun").text(noun);
+    blanks.forEach(function(blank) {
+      var input = $("input#" + blank).val();
+      $("." + blank).text(input);
+    });
 
     $("#story").show();
 
